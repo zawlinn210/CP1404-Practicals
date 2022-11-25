ed80f657ac37cdfb734c3755543f1b5b11ae47b3
Zaw Linn <zawlinnhtunnaing@my.jcu.edu.au>
2022-11-25 15:17:36 +0630

    Refactor password check program with functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,21 @@
-password = input("Enter password: ")
-while len(password) < 4:
-    print("Password Too Short!")
+def main():
+    """Get password and print asterisks"""
+    password = get_password()
+    show_asterisks(password)
+
+
+def get_password():
+    """Get password"""
     password = input("Enter password: ")
-print(len(password) * "*")
+    while len(password) < 4:
+        print("Password Too Short!")
+        password = input("Enter password: ")
+    return password
+
+
+def show_asterisks(password):
+    """Print asterisks"""
+    print(len(password) * "*")
+
+
+main()
