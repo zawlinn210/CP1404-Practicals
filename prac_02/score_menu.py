6ea5e9c9d3eee013da59dac9e495a34251311a04
Zaw Linn <zawlinnhtunnaing@my.jcu.edu.au>
2022-11-25 16:06:47 +0630

    Refactor score menu program for readability

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -16,30 +16,52 @@ while choice != <quit option>
 <do final thing, if needed>
 """
 MENU = """(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "G":
-        get_score = float(input("Enter score: "))
-        while get_score < 0 or get_score > 100:
-            print("Invalid score!")
-            get_score = float(input("Enter score: "))
-        print("Valid Score!")
-    elif choice == "P":
-        score = float(input("Enter score: "))
-        if score < 0 or score > 100:
-            print("Invalid score")
-        elif score >= 90:
-            print("Excellent")
-        elif score >= 50:
-            print("Passable")
-        else:
-            print("Bad")
-    elif choice == "S":
-        score = int(input("Enter score: "))
-        print(score * "*")
-    else:
-        print("Invalid option")
+
+
+def main():
+    """Print menu and verify choice"""
     print(MENU)
     choice = input(">>> ").upper()
-print("Farewell")
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+            print("Valid Score!")
+        elif choice == "P":
+            print(determine_score_status(score))
+        elif choice == "S":
+            print_stars(score)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell")
+
+
+def get_valid_score():
+    """Get valid score"""
+    get_score = float(input("Enter score: "))
+    while get_score < 0 or get_score > 100:
+        print("Invalid score!")
+        get_score = float(input("Enter score: "))
+    return int(get_score)
+
+
+def determine_score_status(score):
+    """Determine score status"""
+    if score < 0 or score > 100:
+        status = "Invalid score"
+    elif score >= 90:
+        status = "Excellent"
+    elif score >= 50:
+        status = "Passable"
+    else:
+        status = "Bad"
+    return status
+
+
+def print_stars(score):
+    """Print stars"""
+    print(score * "*")
+
+
+main()
