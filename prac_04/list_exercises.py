abf0a330c89707c3f29d703423fd091c84a6dcd3
Zaw Linn <zawlinnhtunnaing@my.jcu.edu.au>
2022-12-04 17:02:16 +0630

    Add input username program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,3 +8,12 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the number is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter your username: ")
+if username not in usernames:
+    print("Access denied")
+else:
+    print("Access granted")
