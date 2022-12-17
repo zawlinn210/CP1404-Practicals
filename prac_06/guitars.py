9d443e6fa31da17e399850ef39e046286ad983c9
Zaw Linn <zawlinnhtunnaing@my.jcu.edu.au>
2022-12-17 17:01:47 +0630

    Complete playing the guitars exercise

---
+++ prac_06/guitars.py
@@ -0,0 +1,31 @@
+"""
+Playing the Guitars
+Estimate: 20 minutes
+Actual:   25 minutes
+"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Print guitar from the created list."""
+    guitars = []
+    print("My guitars!")
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(new_guitar, "added.")
+        name = input("Name: ")
+
+    print("These are my guitars: ")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = "(vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
