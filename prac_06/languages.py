910c1dde9e16089668aa2c23c2422f0d8c661ca6
Zaw Linn <zawlinnhtunnaing@my.jcu.edu.au>
2022-12-17 15:12:09 +0630

    Add client code of Programming Language Class

---
+++ prac_06/languages.py
@@ -0,0 +1,24 @@
+"""
+Client code of ProgrammingLanguage Class.
+Estimate: 15 minutes
+Actual:   18 minutes
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    """Print Fields, Typing, Reflection, and Year."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    print(ruby)
+    print(visual_basic)
+    print("The dynamically typed languages are: ")
+    ProgrammingLanguage.is_dynamic(python)
+    ProgrammingLanguage.is_dynamic(ruby)
+    ProgrammingLanguage.is_dynamic(visual_basic)
+
+
+main()
